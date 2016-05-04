72576ba9a2ae42ec3b5b94a9dfe487a787c37456
Enmaai <softanywhere@126.com>
2016-05-04 14:28:11 +0800

    1

--- www/ibizmobkit.js
+++ www/ibizmobkit.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 
 module.exports = {
     
-    get: function(success, fail) {
+    deviceId: function(success, fail) {
         cordova.exec(success, fail, 'IBizMobKit', 'getDeviceId', []);
     }
 };
